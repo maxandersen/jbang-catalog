3fb02f4a2a96190598ec0730b95ec8f02a871b47
Lukas Eder <lukas.eder@gmail.com>
2021-06-11 14:47:16 +0200

    [#11492] Add a j.u.l.Logger format if not already present

--- codegen.java
+++ codegen.java
@@ -7,6 +7,9 @@ import static java.lang.System.*;
 public class codegen {
 
     public static void main(String... args) throws Exception {
+        if (System.getProperty("java.util.logging.SimpleFormatter.format") == null)
+            System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
+
         org.jooq.codegen.GenerationTool.main(args);
     }
 }
