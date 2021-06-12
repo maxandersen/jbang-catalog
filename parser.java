87ee0f88433ef8521a9dbc601b62f7ddd202ed58
Lukas Eder <lukas.eder@gmail.com>
2021-06-12 10:26:02 +0200

    [#11492] Add ParserCLI and DiffCLI to jbang-catalog

---
+++ parser.java
@@ -0,0 +1,14 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS org.jooq:jooq:${jooq.version:RELEASE}
+
+import static java.lang.System.*;
+
+public class parser {
+
+    public static void main(String... args) throws Exception {
+        if (System.getProperty("java.util.logging.SimpleFormatter.format") == null)
+            System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
+
+        org.jooq.ParserCLI.main(args);
+    }
+}
