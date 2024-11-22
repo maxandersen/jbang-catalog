e0fb195e8e4b0cc3569a3cbb02889bb5ee2b5a40
Max Rydahl Andersen <max@xam.dk>
2024-11-22 13:50:25 +0100

    add desktop option

--- jdbc.java
+++ jdbc.java
@@ -3,17 +3,18 @@
 //DESC This script launches sqlline or h2 console by just specifying jdbc url.
 //DESC `jdbc` will use jbang to download the required driver and launch sqlline or h2 console.
 
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 // jdbc urls: https://www.baeldung.com/java-jdbc-url-format
 // maven drivers: https://vladmihalcea.com/jdbc-driver-maven-dependency/
@@ -31,8 +32,16 @@ class jdbc implements Callable<Integer> {
     @Parameters(index = "1..N", description = "Additional args to pass to sqlline or h2 web console")
     List<String> additionalArgs = List.of();
 
-    @Option(names = "-w", description = "Launch h2 web console instead of sqlline")
-    boolean web;
+    static class LaunchType {
+        @Option(names = "-w", description = "Launch h2 web console instead of sqlline")
+        boolean web;
+
+        @Option(names = "-d", description = "Launch jdbcnav desktop app")
+        boolean desktop;
+    }
+
+    @ArgGroup(exclusive = true)
+    LaunchType launchType;
 
     @Option(names = { "-u", "--user" }, description = "User to connect to database")
     String user;
@@ -76,7 +85,7 @@ class jdbc implements Callable<Integer> {
         }
         command.add("--deps");
         command.add(String.join(",",driverDependency));
-        if(web) {
+        if(launchType.web) {
             command.add("h2@jbanghub/h2");
             if(user != null) {
                 command.add("-user");
@@ -88,6 +97,18 @@ class jdbc implements Callable<Integer> {
             }
             command.add("-url");
             command.add(jdbcurl);
+        } else if(launchType.desktop) {
+            command.add("https://thomasokken.com/jdbcnav/download/jdbcnav.jar");
+            if(user != null) {
+                command.add("-user=%s".formatted(user));
+            }
+            if(password != null) {
+                command.add("-pass=%s".formatted(password));
+            }
+           // command.add("-name=%s".formatted(jdbcuri.getHost()));
+           // command.add("-driver=%s".formatted(setDriverClasses().get(scheme)));
+            command.add("-url=%s".formatted(jdbcurl));
+            
         } else {
             command.add("sqlline:sqlline:RELEASE");
             if(user != null) {
@@ -126,6 +147,7 @@ class jdbc implements Callable<Integer> {
         drivers.put("mysql", List.of("mysql:mysql-connector-java:RELEASE"));
         //https://jdbc.postgresql.org/documentation/head/connect.html
         drivers.put("postgresql", List.of("org.postgresql:postgresql:RELEASE"));
+        drivers.put("postgres", List.of("org.postgresql:postgresql:RELEASE"));
         //https://docs.oracle.com/en/database/oracle/oracle-database/19/jjdbc/JDBC-driver-connection-url-syntax.html#GUID-0A7E1701-2CEC-4608-A498-2D72AEB4013B
         drivers.put("oracle", List.of("com.oracle.database.jdbc:ojdbc10:RELEASE"));
         //https://docs.microsoft.com/en-us/sql/connect/jdbc/microsoft-jdbc-driver-for-sql-server?view=sql-server-ver15
@@ -144,6 +166,36 @@ class jdbc implements Callable<Integer> {
         //https://db.apache.org/derby/docs/10.8/devguide/cdevdvlp17453.html
         drivers.put("derby", List.of("org.apache.derby:derby:RELEASE"));
         drivers.put("sqlite", List.of("org.xerial:sqlite-jdbc:RELEASE", "org.slf4j:slf4j-simple:1.7.36"));
+        return drivers;
+    }
+
+    private Map<String, String> setDriverClasses() {
+        Map<String, String> drivers = new HashMap<>();
+        //https://mariadb.com/kb/en/mariadb-connector-j/
+        drivers.put("mariadb", "org.mariadb.jdbc.Driver");
+        //https://dev.mysql.com/doc/connector-j/8.0/en/
+        drivers.put("mysql", "com.mysql.cj.jdbc.Driver");
+        //https://jdbc.postgresql.org/documentation/head/connect.html
+        drivers.put("postgresql", "org.postgresql.Driver");
+        drivers.put("postgres", "org.postgresql.Driver");
+        //https://docs.oracle.com/en/database/oracle/oracle-database/19/jjdbc/JDBC-driver-connection-url-syntax.html#GUID-0A7E1701-2CEC-4608-A498-2D72AEB4013B
+        drivers.put("oracle", "oracle.jdbc.OracleDriver");
+        //https://docs.microsoft.com/en-us/sql/connect/jdbc/microsoft-jdbc-driver-for-sql-server?view=sql-server-ver15
+        drivers.put("sqlserver", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        //https://help.sap.com/viewer/0eec0d68141541d1b07893a39944924e/2.0.02/en-US/109397c2206a4ab2a5386d494f4cf75e.html
+        drivers.put("sap", "com.sap.db.jdbc.Driver");
+        //https://www.ibm.com/docs/en/informix-servers/14.10?topic=SSGU8G_14.1.0/com.ibm.jdbc_pg.doc/ids_jdbc_501.htm
+        drivers.put("informix", "com.informix.jdbc.IfxDriver");
+        //https://www.firebirdsql.org/file/documentation/drivers_documentation/java/3.0.7/firebird-classic-server.html
+        drivers.put("firebird", "org.firebirdsql.jdbc.FBDriver");
+        drivers.put("firebirdsql", "org.firebirdsql.jdbc.FBDriver");
+        //https://hsqldb.org/doc/2.0/guide/dbproperties-chapt.html
+        drivers.put("hsqldb", "org.hsqldb.jdbc.JDBCDriver");
+        //https://www.h2database.com/html/features.html#database_url
+        drivers.put("h2", "org.h2.Driver");
+        //https://db.apache.org/derby/docs/10.8/devguide/cdevdvlp17453.html
+        drivers.put("derby", "org.apache.derby.jdbc.EmbeddedDriver");
+        drivers.put("sqlite", "org.sqlite.JDBC");
         return drivers;
     }
 }
