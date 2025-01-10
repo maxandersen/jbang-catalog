dea8134ca6404190210b25e14398d01b689c1376
Max Rydahl Andersen <max@xam.dk>
2025-01-10 16:35:56 +0100

    add mpc calculator

---
+++ mcpcalc/mpccalculator.java
@@ -0,0 +1,32 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.quarkus:quarkus-bom:3.17.6@pom
+//DEPS io.quarkiverse.mcp:quarkus-mcp-server-stdio:1.0.0.Alpha2
+//DEPS io.quarkus:quarkus-qute
+//FILES application.properties
+
+import io.quarkiverse.mcp.server.Tool;
+import io.quarkiverse.mcp.server.ToolArg;
+
+public class mpccalculator {
+
+    @Tool(description = "Performs basic arithmetic operations (add, subtract, multiply, divide)")
+    String operation(@ToolArg(description = "The operation to perform - add, subtract, multiply, divide") String operation, 
+                    @ToolArg(description = "The first operand") double a, 
+                    @ToolArg(description = "The second operand") double b) {
+
+                        
+        switch (operation.toLowerCase()) {
+            case "add": return String.valueOf(a + b);
+            case "subtract": return String.valueOf(a - b);
+            case "multiply": return String.valueOf(a * b);
+            case "divide": {
+                if (b == 0) {
+                    return "Division by zero is not allowed!";
+                }
+                return String.valueOf(a / b);
+            }
+        }
+
+        return "Unknown operation!";
+    }
+}
